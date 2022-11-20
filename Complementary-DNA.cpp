38ed495c4a920a824db4feced01173302887f9b2
Loris Prataiolo <73251774+Niotahely@users.noreply.github.com>
2022-11-20 16:09:45 +0100

    Create Complementary-DNA.cpp
    Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
    
    If you want to know more: http://en.wikipedia.org/wiki/DNA
    
    In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
    
    More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)

---
+++ Complementary-DNA.cpp
@@ -0,0 +1,46 @@
+ 
+// personal solution
+ #include <string>
+
+std::string DNAStrand(const std::string& dna)
+{
+  std :: string sol ;
+  
+  if (dna.size() == 0) return "ERROR";
+   
+  for( char c : dna) {
+
+    switch (c){
+        case 'A':
+          sol += 'T'; break;
+        case 'T':
+          sol += 'A'; break;
+        case 'C':
+          sol += 'G'; break;
+        case 'G':
+          sol += 'C'; break;
+    }
+  }
+  
+  return sol
+ 
+ 
+ // optimal solution
+ #include <map>
+
+const std::map<char, char> mapping =
+{
+  {'A', 'T'},
+  {'T', 'A'},
+  {'C', 'G'},
+  {'G', 'C'},
+};
+
+std::string DNAStrand(std::string dna)
+{
+  for (char& c : dna)
+  {
+    c = mapping.at(c);
+  }
+  return dna;
+}
