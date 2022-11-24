52a7ff6118f1f1ea3e9eca049ee566ef44c0e897
Loris Prataiolo <73251774+Niotahely@users.noreply.github.com>
2022-11-24 19:17:48 +0100

    Update Who-likes-it.cpp

--- Who-likes-it.cpp
+++ Who-likes-it.cpp
@@ -5,15 +5,15 @@ std::string likes(const std::vector<std::string> &names)
   switch((int)names.size()){
       
       case 0 :
-        return "no one likes this"; break ;
+        return "no one likes this";
       case 1 :
-        return names[0]+" likes this" ; break;
+        return names[0]+" likes this" ;
       case 2 :
-        return names[0]+" and "+names[1]+" like this"; break ;
+        return names[0]+" and "+names[1]+" like this"; 
       case 3 :
-        return names[0]+", "+names[1]+" and "+names[2] +" like this"; break ;
+        return names[0]+", "+names[1]+" and "+names[2] +" like this"; 
       default :
-        return names[0]+", "+names[1]+" and "+ std::to_string(size-2) +" others like this"; break ;
+        return names[0]+", "+names[1]+" and "+ std::to_string(size-2) +" others like this";
   
   }
 }
